d5d8614d4f690e3e7f5ddd3375f19c2c0c4b6aea
paulr093 <paulrichan@icloud.com>
2022-10-02 07:46:41 -0700

    Initial Commit

---
+++ MacrosSwift/MacrosSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  MacrosSwiftApp.swift
+//  MacrosSwift
+//
+//  Created by Paul Richan on 10/2/22.
+//
+
+import SwiftUI
+
+@main
+struct MacrosSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
